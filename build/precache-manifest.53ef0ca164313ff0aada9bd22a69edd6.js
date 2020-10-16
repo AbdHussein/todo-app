092d45abc4a2cb8f97cba4c115d3a8d2c56846df
AbdHussein <abdulrahmaanhussein97@gmail.com>
2020-10-17 00:09:54 +0300

    V 1.0

---
+++ build/precache-manifest.53ef0ca164313ff0aada9bd22a69edd6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c8138d9c57d3774318d69033e142599",
+    "url": "/index.html"
+  },
+  {
+    "revision": "85c4f65a05dd4f20c24d",
+    "url": "/static/css/main.6840fade.chunk.css"
+  },
+  {
+    "revision": "f41d890973c280b00b0a",
+    "url": "/static/js/2.df171077.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.df171077.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85c4f65a05dd4f20c24d",
+    "url": "/static/js/main.25dc7e45.chunk.js"
+  },
+  {
+    "revision": "559ff28579f192bdf8b9",
+    "url": "/static/js/runtime-main.98bec8e4.js"
+  }
+]);
